491176ae105f951a7aeba86fd5bf1daa1bae5a19
Naman <hikom563@gmail.com>
2024-04-18 10:28:58 +0530

    first commit

---
+++ src/components/SongPlaylist.jsx
@@ -0,0 +1,51 @@
+import { createRandomSong } from "../data";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { addSong, removeSong } from "../store";
+
+function SongPlaylist() {
+  const dispatch = useDispatch();
+  const songPlaylist = useSelector((state) => state.songs);
+
+  const handleSongAdd = (song) => {
+    // To Do:
+    // Add song to list of songs
+    dispatch(addSong(song));
+  };
+  const handleSongRemove = (song) => {
+    dispatch(removeSong(song));
+  };
+
+  const renderedSongs = songPlaylist.map((song) => {
+    return (
+      <li key={song}>
+        {song}
+        <button
+          onClick={() => handleSongRemove(song)}
+          className="button is-danger"
+        >
+          X
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="content">
+      <div className="table-header">
+        <h3 className="subtitle is-3">Song Playlist</h3>
+        <div className="buttons">
+          <button
+            onClick={() => handleSongAdd(createRandomSong())}
+            className="button is-link"
+          >
+            + Add Song to Playlist
+          </button>
+        </div>
+      </div>
+      <ul>{renderedSongs}</ul>
+    </div>
+  );
+}
+
+export default SongPlaylist;
